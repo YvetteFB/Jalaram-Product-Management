f44a2c94377d5908c523f28737f386a9ff32d0b4
Fiona Yvette Baraza <fionabaraza@gmail.com>
2023-03-03 13:09:49 +0300

    navbar styling

--- product_management/settings.py
+++ product_management/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'django.contrib.humanize',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
